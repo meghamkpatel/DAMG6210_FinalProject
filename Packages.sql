b979dd951a11cd1a327959a4bba9f3547622615b
godadesonali <124474704+godadesonali@users.noreply.github.com>
2023-04-13 12:34:44 -0400

    Add files via upload

---
+++ Packages.sql
@@ -0,0 +1,11 @@
+﻿
+
+----Indexes
+CREATE INDEX idx_genre_name ON genre (genreName);
+
+Create index inx_movie_id ON watch_history (movieID);
+
+CREATE INDEX idx_movie_id on genre (movie_id);
+
+CREATE INDEX inx_movie_Title on Movie (movieTitle);
+
