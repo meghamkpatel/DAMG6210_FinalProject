16fc8ec3ee9b07dc3c6da1692dd0b6e0c0c2ebf2
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2023-04-13 11:59:18 -0400

    Update Database_Roles_Script.sql

--- Database_Roles_Script.sql
+++ Database_Roles_Script.sql
@@ -63,3 +63,18 @@ GRANT SELECT, INSERT, UPDATE ON db_admin.subtitles TO DB_MANAGER;
 GRANT SELECT, INSERT, UPDATE ON db_admin.watch_history TO DB_MANAGER;
 GRANT SELECT, INSERT, UPDATE ON db_admin.watchlist TO DB_MANAGER;
 GRANT SELECT,INSERT, UPDATE ON db_admin.customer TO DB_MANAGER;
+
+create sequence movie_seq;
+create sequence customer_seq;
+create sequence genre_seq;
+create sequence director_seq;
+create sequence watchlist_seq;
+create sequence favorite_seq;
+create sequence download_seq;
+create sequence watchhistory_seq;
+create sequence address_seq;
+create sequence subtitle_seq;
+create sequence actor_seq;
+create sequence purchase_seq;
+create sequence region_seq;
+create sequence plan_seq;
