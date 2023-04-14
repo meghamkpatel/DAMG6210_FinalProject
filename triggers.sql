044b0300280be3578f95495cba84665fa6ed1b8b
utk-n <124312673+utk-n@users.noreply.github.com>
2023-04-14 02:12:32 -0400

    Update triggers.sql
    Added 2 triggers

--- triggers.sql
+++ triggers.sql
@@ -19,8 +19,37 @@ END;
 /
 
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+--changes the status of the customer upon purchasing a new plan
+CREATE OR REPLACE TRIGGER set_status
+AFTER INSERT ON Purchase
+FOR EACH ROW
+BEGIN
+  UPDATE customer c
+  SET c.customerstatus = 'Active'
+  WHERE c.customerid = :NEW.customerid;
+END;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Deletes the rows in watch list whenever an occurance of the same details are added in the watch history table 
+CREATE OR REPLACE TRIGGER delete_watchlist_row
+AFTER INSERT ON watch_history
+FOR EACH ROW
+BEGIN
+  DELETE FROM watchlist
+  WHERE customerid = :NEW.customerid
+    AND movieid = :NEW.movieid;
+END;
+
+
+--testing the trigger
+/*INSERT INTO watch_history (HistoryID, MovieID, CustomerID, DateWatched)
+SELECT MAX(HistoryID) + 1, 57, 109, SYSDATE
+FROM watch_history;*/
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
