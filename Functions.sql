d204a2f80aaa1ed73da5a831a92c4057b154f008
utk-n <124312673+utk-n@users.noreply.github.com>
2023-04-14 02:15:03 -0400

    Update Functions.sql
    added get_screen_limit_by_customerid

--- Functions.sql
+++ Functions.sql
@@ -190,3 +190,23 @@ BEGIN
   new_purchase_id := auto_renew_plan(3, 123); -- replace with your plan_id and customer_id values
 END;*/ 
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION get_screenlimit_by_customer(customer_id NUMBER)
+RETURN screen_limit_row AS
+  screenlimit_info screen_limit_row;
+BEGIN
+ Select screen_limit_row(p.customerid,c.userfirstname,s.planname,s.screenlimit)
+ into screenlimit_info
+ from purchase p
+ join subscription_plan s
+ on p.planid = s.planid
+ join customer c
+ on c.customerid = p.customerid
+ where p.customerid = customer_id; 
+RETURN screenlimit_info;
+END;
+
+
+--SELECT get_screenlimit_by_customer(25).customerid, get_screenlimit_by_customer(25).customername, get_screenlimit_by_customer(25).planname, get_screenlimit_by_customer(25).screenlimit
+--FROM dual;
