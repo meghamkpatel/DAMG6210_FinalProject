b926059bd2ace7848f61cc4e1685ddeb7a1cb343
utk-n <124312673+utk-n@users.noreply.github.com>
2023-04-14 01:25:32 -0400

    Update screenlimit_function.sql
    adding comment lines on function call

--- screenlimit_function.sql
+++ screenlimit_function.sql
@@ -1,13 +1,3 @@
-
-
-
-CREATE OR REPLACE TYPE screen_limit_row AS OBJECT(
-  customerid NUMBER,
-  customername VARCHAR(35),
-  planname VARCHAR(50),
-  screenlimit NUMBER
-);
-
 CREATE OR REPLACE FUNCTION get_screenlimit_by_customer(customer_id NUMBER)
 RETURN screen_limit_row AS
   screenlimit_info screen_limit_row;
@@ -23,13 +13,11 @@ BEGIN
 RETURN screenlimit_info;
 END;
 
-SELECT get_screenlimit_by_customer(25).customerid, get_screenlimit_by_customer(25).customername, get_screenlimit_by_customer(25).planname, get_screenlimit_by_customer(25).screenlimit
-FROM dual;
+
+--SELECT get_screenlimit_by_customer(25).customerid, get_screenlimit_by_customer(25).customername, get_screenlimit_by_customer(25).planname, get_screenlimit_by_customer(25).screenlimit
+--FROM dual;
 
 
-
-select * from purchase where purchase.customerid = 215
-select * from customer where customerid = 215
 
 
 
