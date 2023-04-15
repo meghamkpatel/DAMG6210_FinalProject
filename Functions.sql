c6833a304a0c92c5827448ae218fac065836be4f
disha-sanil <122700176+disha-sanil@users.noreply.github.com>
2023-04-14 21:00:59 -0400

    Update Functions.sql
    Rectified get_screenlimit_by_customer function

--- Functions.sql
+++ Functions.sql
@@ -209,21 +209,31 @@ END;*/
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION get_screenlimit_by_customer(customer_id NUMBER)
-RETURN screen_limit_row AS
-  screenlimit_info screen_limit_row;
+CREATE OR REPLACE FUNCTION get_screenlimit_by_customer (
+    customer_id NUMBER
+) RETURN screen_limit_row AS
+    screenlimit_info screen_limit_row;
 BEGIN
- Select screen_limit_row(p.customerid,c.userfirstname,s.planname,s.screenlimit)
- into screenlimit_info
- from purchase p
- join subscription_plan s
- on p.planid = s.planid
- join customer c
- on c.customerid = p.customerid
- where p.customerid = customer_id; 
-RETURN screenlimit_info;
+    SELECT
+        screen_limit_row(p.customerid, c.userfirstname, s.planname, s.screenlimit)
+    INTO screenlimit_info
+    FROM
+             purchase p
+        JOIN subscription_plan s ON p.planid = s.planid
+        JOIN customer          c ON c.customerid = p.customerid
+    WHERE
+        p.customerid = customer_id
+    ORDER BY
+        p.startdate DESC
+    FETCH FIRST 1 ROW ONLY;
+    RETURN screenlimit_info;
 END;
 
-
---SELECT get_screenlimit_by_customer(25).customerid, get_screenlimit_by_customer(25).customername, get_screenlimit_by_customer(25).planname, get_screenlimit_by_customer(25).screenlimit
---FROM dual;
+-- Function call
+/*SELECT
+    get_screenlimit_by_customer(103).customerid,
+    get_screenlimit_by_customer(103).customername,
+    get_screenlimit_by_customer(103).planname,
+    get_screenlimit_by_customer(103).screenlimit
+FROM
+    dual;*/
